06a6d9f586bc6b72aef0d7c72a0145620c73f505
jakekirsch2 <jakekirsch11@gmail.com>
2022-09-10 21:19:56 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import os
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST'])
+def main():
+    message = request.get_json(force=True)
+    company = message['company']
+    file_url = message['file_url']
+    file_name = message['file_name']
+    os.system(f" curl {file_url} | gsutil cp - gs://data-platform-data/{company}/{file_name}")
+    return "SUCCESS"
+    
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
