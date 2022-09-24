93d88bcf1a6447670d14b16905fb519711e1b299
jakekirsch2 <jakekirsch11@gmail.com>
2022-09-24 13:42:22 -0500

    changing code

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #curl -m 3610 -X POST https://add-hc-pricing-data-gwmmhrzkra-uc.a.run.app -H "Authorization: bearer $(gcloud auth print-identity-token)" -H "Content-Type: application/json" -d '{"company": "uhc", "file_url": "https://uhc-tic-mrf.azureedge.net/public-mrf/2022-09-01/2022-09-01_Bind-Benefits--Inc-_TPA_UNITEDHEALTHCARE-CHOICE-PLUS_UCQ_in-network-rates.json.gz", "file_name": "2022-09-01_Bind-Benefits--Inc-_TPA_UNITEDHEALTHCARE-CHOICE-PLUS_UCQ_in-network-rates.json.gz"}'
+#curl -m 3610 -X POST http://10.65.0.2:8000 -H "Authorization: bearer $(gcloud auth print-identity-token)" -H "Content-Type: application/json" -d '{"company": "uhc", "file_url": "https://uhc-tic-mrf.azureedge.net/public-mrf/2022-09-01/2022-09-01_Bind-Benefits--Inc-_TPA_UNITEDHEALTHCARE-CHOICE-PLUS_UCQ_in-network-rates.json.gz", "file_name": "2022-09-01_Bind-Benefits--Inc-_TPA_UNITEDHEALTHCARE-CHOICE-PLUS_UCQ_in-network-rates.json.gz"}'
+
 
 from flask import Flask, request
 import os
@@ -6,12 +8,13 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def main():
-    message = request.get_json(force=True)
-    company = message['company']
-    file_url = message['file_url']
-    file_name = message['file_name']
-    os.system(f" curl '{file_url}' | gsutil cp - gs://data-platform-data/{company}/{file_name}")
-    return "SUCCESS"
+    return "Hello Jake"
+    # message = request.get_json(force=True)
+    # company = message['company']
+    # file_url = message['file_url']
+    # file_name = message['file_name']
+    # os.system(f" curl '{file_url}' | gsutil cp - gs://data-platform-data/{company}/{file_name}")
+    # return "SUCCESS"
     #wget 
     
 
