ca014ad4ab9e0dd840b3ba1e598ca9c17e330e7b
jakekirsch2 <jakekirsch11@gmail.com>
2023-11-26 15:46:13 -0600

    back to basic

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
-#curl -m 3610 -X POST https://add-hc-pricing-data-mnirkpcsna-uc.a.run.app/get-cigna -H "Authorization: bearer $(gcloud auth print-identity-token)" -H "Content-Type: application/json" -d '{"url": "https://d25kgz5rikkq4n.cloudfront.net/cost_transparency/mrf/table-of-contents/reporting_month=2023-11/2023-11-01_cigna-health-life-insurance-company_index.json?Expires=1702688709&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kMjVrZ3o1cmlra3E0bi5jbG91ZGZyb250Lm5ldC9jb3N0X3RyYW5zcGFyZW5jeS9tcmYvdGFibGUtb2YtY29udGVudHMvcmVwb3J0aW5nX21vbnRoPTIwMjMtMTEvMjAyMy0xMS0wMV9jaWduYS1oZWFsdGgtbGlmZS1pbnN1cmFuY2UtY29tcGFueV9pbmRleC5qc29uIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzAyNjg4NzA5fX19XX0_&Signature=LNxqa5JiDXUbaVg6itmXp4dKHLtw7YCOT3kSgTJSjBK4ZJrOZUtPXYJKHHoAHq0NyZzKYXlD-XZJgXwmxeYXPDY8kQgh9P~RULCP8ugpffhasB2eHrDN0xXi6qQFYHXAJiKT0o04gCcs1CT1jg11JfeRKmv5cFxWXq4hoSJ~dqCwnMcE6fwFGZbSr88I8xBz0oJMOrvuXr2MHsaV2rNRsooqhtJwO-EfbNDK4NCMqy9xUD~Fv-s~c8e6w53MX2-2KS3oU~OvF5SZpNEE46LT4nsnlyCRJ1BA01JVDOvZgibzJXETLOVCge3PYScah-ddBLg4prLNQCbEj4ZNB~Kpwg__&Key-Pair-Id=K1NVBEPVH9LWJP"}'
+#curl -m 3610 -X POST https://add-hc-pricing-data-gwmmhrzkra-uc.a.run.app -H "Authorization: bearer $(gcloud auth print-identity-token)" -H "Content-Type: application/json" -d '{"company": "uhc", "file_url": "https://uhc-tic-mrf.azureedge.net/public-mrf/2022-09-01/2022-09-01_Bind-Benefits--Inc-_TPA_UNITEDHEALTHCARE-CHOICE-PLUS_UCQ_in-network-rates.json.gz", "file_name": "2022-09-01_Bind-Benefits--Inc-_TPA_UNITEDHEALTHCARE-CHOICE-PLUS_UCQ_in-network-rates.json.gz"}'
 
 from flask import Flask, request
 import os
-from cigna.parse_file import parse_file
 app = Flask(__name__)
 
-@app.route('/get-cigna', methods = ['POST'])
-def get_cigna():
+@app.route('/', methods = ['POST'])
+def main():
     message = request.get_json(force=True)
-    url = message['url']
-    data = parse_file(url)
-    for link in data:
-        #get file name from link https://d25kgz5rikkq4n.cloudfront.net/cost_transparency/mrf/table-of-contents/reporting_month=2023-11/2023-11-01_cigna-health-life-insurance-company_index.json?Expires=1702688709&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kMjVrZ3o1cmlra3E0bi5jbG91ZGZyb250Lm5ldC9jb3N0X3RyYW5zcGFyZW5jeS9tcmYvdGFibGUtb2YtY29udGVudHMvcmVwb3J0aW5nX21vbnRoPTIwMjMtMTEvMjAyMy0xMS0wMV9jaWduYS1oZWFsdGgtbGlmZS1pbnN1cmFuY2UtY29tcGFueV9pbmRleC5qc29uIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzAyNjg4NzA5fX19XX0_&Signature=LNxqa5JiDXUbaVg6itmXp4dKHLtw7YCOT3kSgTJSjBK4ZJrOZUtPXYJKHHoAHq0NyZzKYXlD-XZJgXwmxeYXPDY8kQgh9P~RULCP8ugpffhasB2eHrDN0xXi6qQFYHXAJiKT0o04gCcs1CT1jg11JfeRKmv5cFxWXq4hoSJ~dqCwnMcE6fwFGZbSr88I8xBz0oJMOrvuXr2MHsaV2rNRsooqhtJwO-EfbNDK4NCMqy9xUD~Fv-s~c8e6w53MX2-2KS3oU~OvF5SZpNEE46LT4nsnlyCRJ1BA01JVDOvZgibzJXETLOVCge3PYScah-ddBLg4prLNQCbEj4ZNB~Kpwg__&Key-Pair-Id=K1NVBEPVH9LWJP
-        file_name = link.split("/")
-        file_name = file_name[-3] + "/" + file_name[-2] + "/" + file_name[-1]
-        file_name = file_name.split("?")[0]
-        #check if file already exists
-        if os.system(f"gsutil ls gs://healthcare-raw-files/unacked/cigna/{file_name}") == 0:
-            print(f"Downloading {file_name} from {link}")
-            os.system(f" curl '{link}' | gsutil cp - gs://healthcare-raw-files/unacked/cigna/{file_name}")
+    company = message['company']
+    file_url = message['file_url']
+    file_name = message['file_name']
+    os.system(f" curl '{file_url}' | gsutil cp - gs://data-platform-data/{company}/{file_name}")
     return "SUCCESS"
     #wget 
     
